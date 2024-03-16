b3a3a70c7632f4ee3683fe75cf267a8e0134deba
mateu52 <wuoelte@gmail.com>
2024-03-16 11:46:29 +0100

    use client fetching

--- app/(private)/home/page.tsx
+++ app/(private)/home/page.tsx
@@ -1,7 +1,55 @@
+
+"use client"
+import { useEffect, useState } from "react";
+
+type Review = {
+    id: string;
+    name: string;
+}
+type AirtableReviewResponseDto = {
+    records: {
+        id: string;
+        fields: {
+            name: string;
+        }
+    }[];
+}
 const Home = () => {
+    const [ reviews, setReviews ] = useState<Review[] | undefined>(undefined);
+    useEffect(() => {
+        fetch('https://api.airtable.com/v0/appZLJE69lV4Vugrp/identable?view=default',{
+            headers: {
+                Authorization: `Bearer ${process.env.API_KEY}`
+            }
+        })
+        .then((response) => response.json())
+        .then((data: AirtableReviewResponseDto) => {
+            const reviewsPlus: Review[] = [];
+            data.records.forEach((elem) => {
+                reviewsPlus.push({
+                    id: elem.id,
+                    name:elem.fields.name,
+                })
+            });
+            setReviews(reviewsPlus)
+        })
+        .catch(error => console.log("error", error))
+    },[])
+    console.log(process.env.API_KEY,' hej')
+    console.log(reviews, 'ok')
     return (
-        <h1>Home</h1>
+        <div>
+        <h1>app1</h1>
+        <ul>
+            {reviews?.map((elem) => (
+            <li key={elem.id}>
+                <div>{elem.name}</div>
+            </li>
+        ))}
+        </ul>
+        </div>
     )
+    
 }
 
 export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,54 @@
+'use client'
+import React from "react";
+import { useEffect, useState } from "react";
+
+type Review = {
+    id: string;
+    name: string;
+};
+type AirtableReviewResponseDto = {
+    records: {
+        id: string;
+        fields: {
+            name:string;
+        }
+    }[];
+}
 
 const App = () => {
+    const [ reviews, setReviews ] = useState<Review[] | undefined>(undefined);
+    useEffect(() => {
+        fetch('https://api.airtable.com/v0/appZLJE69lV4Vugrp/identable?view=default',{
+            headers: {
+                Authorization: `Bearer ${process.env.API_KEY}`
+            }
+        })
+        .then((response) => response.json())
+        .then((data: AirtableReviewResponseDto) => {
+            const _reviews: Review[] = [];
+            data.records.forEach((elem) => {
+                _reviews.push({
+                    id: elem.id,
+                    name:elem.fields.name,
+                })
+            });
+            setReviews(_reviews)
+        })
+        
+    },[])
+    console.log(process.env.API_KEY,' hej')
+    console.log(reviews, 'ok')
     return (
-        <h1>app</h1>
+        <div>
+        <h1>app1</h1>
+        <ul>
+            {reviews?.map((elem) => (
+            <li key={elem.id}>
+                <div>{elem.name}</div>
+            </li>
+        ))}
+        </ul>
+        </div>
     )
 }
 
